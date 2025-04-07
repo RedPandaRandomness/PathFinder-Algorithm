8a253b15133a370183b00c116eeffdccb4a27f6c
RedPandaRandomness <lorafrederico@gmail.com>
2025-04-07 09:45:42 -0400

    Create Board.java

---
+++ Board.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class Board here.
+ *
+ * @author (Lora & Benj)
+ * @version (a version number or a date)
+ */
+public class Board
+{
+    // instance variables
+    public static final int WIDTH = 600;
+    public static final int HEIGHT = 600;
+    Tile[][] grid = new Tile[12][12];
+
+    /**
+     * Constructor for objects of class Board
+     */
+    public Board()
+    {
+        // initialise instance variables
+        
+    }
+
+    
+}
