57effd414ef59b2abbde60c3df353aaec9eddffd
BugBoy890 <bford4@ocdsb.ca>
2025-04-08 10:27:29 -0400

    Update Tile.java

--- Tile.java
+++ Tile.java
@@ -1,5 +1,6 @@
 import static java.lang.System.out; //shortcut for writing output
 import java.awt.Rectangle;
+import java.awt.Color;
 
 /**
  * Write a description of class Tile here.
@@ -12,11 +13,13 @@ public class Tile extends Rectangle
     // instance variables
     public static final int WIDTH = 50;
     public static final int HEIGHT = 50;
+    public Color colour;
     public int state = 0;
     /* 0 = unvisited, unmarked
      * 1 = unvisited, marked
      * 2 = visited, unmarked
      * 3 = visited, marked <- impossible?
+     * 4 = tree (ignore)
      */
     
     /**
@@ -25,7 +28,7 @@ public class Tile extends Rectangle
     public Tile(int x, int y)
     {
         super(x,y,WIDTH,HEIGHT);
-        
+        state = 0;
     }
 
     public static void main(String[] args)
@@ -33,13 +36,37 @@ public class Tile extends Rectangle
         
     }
     
-    public int getState(int state)
+    public int getState()
     {
         return 0;
     }
     
-    public int setState(int state)
+    public void setState(int state)
     {
-        return 0;
+        if(state == 0) // unvisited, unmarked
+        {
+            colour = new Color(255,255,255); // white
+            this.state = state;
+        }
+        else if(state == 1) // unvisited, marked
+        {
+            colour = new Color(255,0,0); // red
+            this.state = state;
+        }
+        else if(state == 2) // visited, unmarked
+        {
+            colour = new Color(0,255,0);
+            this.state = state;
+        }
+        else if(state == 3) //# visited, marked
+        {
+            colour = new Color(200,0,255);
+            this.state = state;
+        }
+        else if(state == 4) // tree
+        {
+            colour = new Color(0,0,0); // black
+            this.state = state;
+        }
     }
 }
