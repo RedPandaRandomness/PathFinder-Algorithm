e6de5dfc4242e5a44511adf9a9d97efa6f357a41
RedPandaRandomness <lorafrederico@gmail.com>
2025-04-08 10:29:39 -0400

    Update Board.java
    Draws tiles on board

--- Board.java
+++ Board.java
@@ -1,3 +1,12 @@
+import static java.lang.System.out; //shortcut for writing output
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.BasicStroke;
+
+
 
 /**
  * Write a description of class Board here.
@@ -5,7 +14,7 @@
  * @author (Lora & Benj)
  * @version (a version number or a date)
  */
-public class Board
+public class Board extends JPanel
 {
     // instance variables
     public static final int WIDTH = 600;
@@ -18,8 +27,46 @@ public class Board
     public Board()
     {
         // initialise instance variables
+        setPreferredSize(new Dimension(WIDTH,HEIGHT));
+        setBackground(new Color(211,211,211));
         
+        main();
+    }
+    
+    public void main()
+    {   
+        //make tiles
+        System.out.println("hi");
+        for(int i = 0; i<grid.length; i++){
+            out.println(i);
+            for(int j = 0; j<grid[i].length; j++){
+                out.println(j);
+                grid[i][j] = new Tile(i*50,j*50);
+            }
+        }
     }
 
+    public void paintComponent(Graphics g)
+    {
+        Graphics2D outline = (Graphics2D) g;
+        outline.setColor(Color.black);
+        outline.setStroke(new BasicStroke(3));
+        
     
+        for(int i = 0; i<grid.length; i++){
+            for(int j = 0; j<grid[i].length; j++){
+                g.setColor(grid[i][j].colour);
+                g.fillRect(grid[i][j].x,grid[i][j].y,
+                    grid[i][j].width,grid[i][j].height);
+                
+                outline.setColor(Color.black);
+                outline.setStroke(new BasicStroke(3));
+                outline.drawRect(grid[i][j].x,grid[i][j].y,
+                    grid[i][j].width,grid[i][j].height);
+            }
+        }
+        
+        
+        
+    }
 }
