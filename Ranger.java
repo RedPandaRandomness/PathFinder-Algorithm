21b29a1fa5c13cfbfa568b762396bde881b185a3
RedPandaRandomness <lorafrederico@gmail.com>
2025-04-25 09:44:47 -0400

    Update Ranger.java
    Solved homelessness

--- Ranger.java
+++ Ranger.java
@@ -23,55 +23,56 @@ public class Ranger
     {
         location = new Point(0,0);
         maze = board;
-        
+
     }
-    
+
     public int getFacing()
     {
         return face;
     }
-    
+
     public Point getLocation()
     {
         return location;
     }
-    
+
     public int getX()
     {
         return location.x;
     }
+
     public int getY()
     {
         return location.y;
     }
-    
+
     public void setLocation(Point location)
     {
         this.location = location;
     }
-    
+
     public void setLocation(int x, int y)
     {
         location = new Point(x,y);
     }
-    
+
     public void move(Point move)
     {
         location = new Point(location.x+move.x, location.y+move.y);
     }
-    
+
     public void cPush(Point coor) { //Pushes point to check later
-      ToCheckNode newTop = new ToCheckNode(coor);
-      newTop.setNext(topC);   // The new Node points to the old top.
-      topC = newTop;        // The new item is now on top.
+        ToCheckNode newTop = new ToCheckNode(coor);
+        newTop.setNext(topC);   // The new Node points to the old top.
+        topC = newTop;        // The new item is now on top.
     }
-    
+
     public void pPush(Point coor){
         PathNode newTop = new PathNode(coor);
         newTop.setNext(topP);   // The new Node points to the old top.
         topP = newTop;        // The new item is now on top.
     }
-    
+
     public Point cPop(){
         if (topC == null ){
             throw new IllegalStateException(
@@ -81,11 +82,11 @@ public class Ranger
         topC = topC.getNext();
         return coor;
     }
-    
+
     private boolean cIsEmpty(){
         return (topC == null);
     }
-    
+
     public Point pPop(){
         if (topP == null ){
             throw new IllegalStateException(
@@ -95,82 +96,80 @@ public class Ranger
         topP = topP.getNext(); //New top is old tops pointer
         return coor;
     }
-    
+
     public Point getDist(Point a){
         return new Point (a.x - location.x, a.y - location.y);
     }
-    
+
     public void look()
     {   
-        Point moveTo = new Point(0,0);
+            Point moveTo = new Point(0,0);
+            if(!backtrack){
+                //Check north
+                if(tileValid(location.x, location.y-1)){
+                    if(moveTo.equals(new Point (0,0))){
+                        moveTo = new Point(0,-1);
+                        face = 0;
+                    }
+                    else{
+                        cPush(new Point(location.x,location.y));
+                    }
+                }
+                //Check East
+                if(tileValid(location.x+1, location.y)){
+                    if(moveTo.equals(new Point (0,0))){
+                        moveTo = new Point(1,0);
+                        face = 1;
+                    }
+                    else{
+                        cPush(new Point(location.x,location.y));
+                    }
+                }
+                //Check South
+                if(tileValid(location.x, location.y+1)){
+                    if(moveTo.equals(new Point (0,0))){
+                        moveTo = new Point(0,1);
+                        face = 2;
+                    }
+                    else{
+                        cPush(new Point(location.x,location.y));
+                    }
+                }
+                //Check West
+                if(tileValid(location.x-1, location.y)){
+                    if(moveTo.equals(new Point (0,0))){
+                        moveTo = new Point(-1,0);
+                        face = 3;
+                    }
+                    else{
+                        cPush(new Point(location.x,location.y));
+                    }
+                }
+                maze.grid[location.x][location.y].setState(1);
+                if(!moveTo.equals(new Point(0,0))){pPush(location);}//Add location to path stack
+                move(moveTo);
+            }
+
+            //dead end
+            if(moveTo.equals(new Point(0,0))){
+                backtrack = true;
+                maze.grid[location.x][location.y].setState(2);
+                backtrack();
+            }
         
-        if(!backtrack){
-    
-            //Check north
-            if(tileValid(location.x, location.y-1)){
-                  if(moveTo.equals(new Point (0,0))){
-                    moveTo = new Point(0,-1);
-                    face = 0;
-                }
-                else{
-                    cPush(new Point(location.x,location.y));
-                }
-            }
-            //Check East
-            if(tileValid(location.x+1, location.y)){
-                  if(moveTo.equals(new Point (0,0))){
-                    moveTo = new Point(1,0);
-                    face = 1;
-                }
-                else{
-                    cPush(new Point(location.x,location.y));
-                }
-            }
-            //Check South
-            if(tileValid(location.x, location.y+1)){
-                  if(moveTo.equals(new Point (0,0))){
-                    moveTo = new Point(0,1);
-                    face = 2;
-                }
-                else{
-                    cPush(new Point(location.x,location.y));
-                }
-            }
-            //Check West
-            if(tileValid(location.x-1, location.y)){
-                  if(moveTo.equals(new Point (0,0))){
-                    moveTo = new Point(-1,0);
-                    face = 3;
-                }
-                else{
-                    cPush(new Point(location.x,location.y));
-                }
-            }
-            maze.grid[location.x][location.y].setState(1);
-            if(!moveTo.equals(new Point(0,0))){pPush(location);}//Add location to path stack
-            move(moveTo);
-        }
-        
-        //dead end
-        if(moveTo.equals(new Point(0,0))){
-            backtrack = true;
-            maze.grid[location.x][location.y].setState(2);
-            //System.out.println("Backtrack now");
-            backtrack();
-        }
-        
+
         maze.repaint();
+    }
+
+    private boolean tileValid(int x, int y){
+        if(x>=0 && x<=11 && y>=0 && y<=11 &&(
+            maze.grid[x][y].getState() == 0 || 
+            maze.grid[x][y].getState() == 3)){
+            return true;
         }
-    
-        private boolean tileValid(int x, int y){
-            if(x>=0 && x<=11 && y>=0 && y<=11 &&(
-                maze.grid[x][y].getState() == 0 || 
-                maze.grid[x][y].getState() == 3)){
-                return true;
-            }
-            else{
-                return false;
-            }
+        else{
+            return false;
+        }
     }
 
     private void backtrack()
@@ -191,8 +190,13 @@ public class Ranger
             else if(direction.y == 1){face = 2;}
             else if(direction.x == -1){face = 3;}
             
-            setLocation(pPop());
+            if(topP.getNext() != null){
+                setLocation(pPop());
+            }
+            else{
+                maze.setBoard(69);
+            }
         }
-        
+
     }
 }
